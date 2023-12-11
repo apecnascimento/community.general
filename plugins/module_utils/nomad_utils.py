805d4bb3163b4f44cd0d5adf7eb4c99c6be0c562
Pedro Nascimento <apecnascimento@gmail.com>
2023-12-11 12:28:38 -0300

    Add transform_token_response

--- plugins/module_utils/nomad_utils.py
+++ plugins/module_utils/nomad_utils.py
@@ -33,6 +33,25 @@ def setup_nomad_client(module):
 
     return nomad_client
 
+def transform_token_response(nomad_response):
+    transformed_response = {
+        "accessor_id": nomad_response['AccessorID'],
+        "create_index": nomad_response['CreateIndex'],
+        "create_time": nomad_response['CreateTime'],
+        "expiration_ttl": nomad_response['ExpirationTTL'],
+        "expiration_time": nomad_response['ExpirationTime'],
+        "global": nomad_response['Global'],
+        "hash": nomad_response['Hash'],
+        "modify_index": nomad_response['ModifyIndex'],
+        "name": nomad_response['Name'],
+        "policies": nomad_response['Policies'],
+        "roles": nomad_response['Roles'],
+        "secret_id": nomad_response['SecretID'],
+        "type": nomad_response['Type']
+    }
+
+    return transformed_response
+
 
 def nomad_auth_argument_spec():
     return dict(
